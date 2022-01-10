d3b1b3415bb8df1dad16eba67e8d204d71e5bf16
Jose M Perez <jmperezvzla@gmail.com>
2022-01-10 17:14:12 -0400

    Center title in Create and Edit Pages

--- resources/js/Pages/Tipos/Create.vue
+++ resources/js/Pages/Tipos/Create.vue
@@ -3,7 +3,7 @@
 
     <BreezeAuthenticatedLayout>
         <template #header>
-            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            <h2 class="text-xl font-semibold leading-tight text-gray-800 text-center">
                 Crear un Tipo
             </h2>
         </template>
